e5e53df46578571f9631b2b89dc5aaa207b9b1eb
chris3edwards3 <chris3edwards3@gmail.com>
2019-04-16 15:52:00 -0600

    Update GFS_descargar_procesar.py

--- GFS_descargar_procesar.py
+++ GFS_descargar_procesar.py
@@ -43,23 +43,13 @@ else:
     os.mkdir(data_folder_path)
 # List of all desired GFS file Id's. We use the 6-hr timesteps, and then the 12.
 
-# The list is shorter, for testing
+# This is the List for 2 days
 file_id_list = ["006", "012", "018", "024", "030", "036", "042", "048"]
-
-# This is the entire list. Don't use for testing (comment out).
-#file_id_list = ["006", "012", "018", "024", "030", "036", "042", "048",
-#                "054", "060", "066", "072", "078", "084", "090", "096",
-#                "102", "108", "114", "120", "126", "132", "138", "144",
-#                "150", "156", "162", "168", "174", "180", "186", "192",
-#                "198", "204", "210", "216", "222", "228", "234", "240",
-#                "252", "264", "276", "288", "300", "312", "324", "336",
-#                "324", "336","348", "360", "372", "384"
-#                ]
 
 # Loop to download and import rasters from the NOMADS database, GFS 0.25 Degree
 for i in range(len(file_id_list)):
     # Download the file to the new raw-data directory
-    data_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t00z.pgrb2.0p25.f" + file_id_list[i] + "&all_lev=on&var_APCP=on&subregion=&leftlon=282&rightlon=299&toplat=24&bottomlat=13.5&dir=%2Fgfs." + input_datetime
+    data_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t06z.pgrb2.0p25.f" + file_id_list[i] + "&all_lev=on&var_APCP=on&subregion=&leftlon=282&rightlon=299&toplat=24&bottomlat=13.5&dir=%2Fgfs." + input_datetime
     filename = "gfs_apcp_" + input_datetime +"_f" + file_id_list[i] + ".grb"
     data_file_path = data_folder_path + "/" + filename
     urllib.request.urlretrieve(data_url, data_file_path)
